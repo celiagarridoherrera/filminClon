79630392dac535b890c2763a4ec1c30aeab76bdb
JoseRD149 <joseromerodiaz89@gmail.com>
2025-01-16 09:05:25 +0100

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+    return (
+        <nav>
+            <h1>Filmin</h1>
+            <ul>
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#cine">Cine</a></li>
+                <li><a href="#series">Series</a></li>
+                <li><a href="#colecciones">Colecciones</a></li>
+            </ul>
+        </nav>
+    );
+}
