4651643314044339f7997ad9417e166086e9f2a0
Lgtf5 <lgtablasf5@gmail.com>
2025-01-22 14:45:58 +0100

    added carousel for slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import { fetchData} from './api';
+import { fetchData } from './api';
 import { useState, useEffect } from 'react';
-
+import Slider from './Components/Slider/Slider';
 
 
 function App() {
-  
+
   const [movies, setMovies] = useState({});
   const page = 6;
 
@@ -14,22 +14,18 @@ function App() {
 
       const storedMovies = localStorage.getItem(`movies_page_${page}`)
       console.log(storedMovies)
-      
-      if(!storedMovies)
-      {
-          try 
-          {
-            const resultado = await fetchData(page);
-            localStorage.setItem(`movies_page_${page}`, JSON.stringify(resultado));
-            console.log("Datos obtenidos en el localStorage:", resultado)
-            setMovies(resultado)
-          } catch (error)
-          {
-            console.error(`Error al conectar a la API: ${error}`)
-            alert("Intente de nuevo más tarde");
-          }          
-      } else 
-      {
+
+      if (!storedMovies) {
+        try {
+          const resultado = await fetchData(page);
+          localStorage.setItem(`movies_page_${page}`, JSON.stringify(resultado));
+          console.log("Datos obtenidos en el localStorage:", resultado)
+          setMovies(resultado)
+        } catch (error) {
+          console.error(`Error al conectar a la API: ${error}`)
+          alert("Intente de nuevo más tarde");
+        }
+      } else {
         setMovies(JSON.parse(storedMovies))
         console.log("Ya hay datos en el localStorage para la página:", page);
         console.log(JSON.parse(storedMovies));
@@ -40,9 +36,9 @@ function App() {
   }, [page])
 
   return (
-    <>
+    <Slider/>
 
-    </>
+
   )
 }
 
