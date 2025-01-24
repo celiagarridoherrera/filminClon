a23db12ae9828fbcacd810dfbb2895bca255ff11
AlejandroAriasSL <alejandroasilva02@gmail>
2025-01-24 01:14:22 +0100

    hotfix and refactor

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import Slider from "./Slider/Slider";
+import { CategoryCarousel } from "./Categories/Categories";
+
+export const HomePage = () => {
+
+    return (
+        <>
+            <Slider page={1}></Slider>
+            <CategoryCarousel page={1} ></CategoryCarousel>
+        </>
+    )
+
+}
